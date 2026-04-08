5f57f8d6993383bf96b2663afc5ec9729f822205
Tanushri Jadhav <tanushrijadhav08@gmail.com>
2026-04-08 14:07:27 +0530

    init : initialised java file

---
+++ StringUtils.java
@@ -0,0 +1,9 @@
+class StringUtils {
+    static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    static String toUpper(String str) {
+        return str.toUpperCase();
+    }
+}+
